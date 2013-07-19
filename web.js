54a712af5efa4bf07344b430ac968ffa596f9a00
Ubuntu <ubuntu@ip-172-31-33-56.us-west-2.compute.internal>
2013-07-19 03:09:28 +0000

    data is buffer..

--- web.js
+++ web.js
@@ -1,16 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var len = 0;
 
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  var buf = data;
-});
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8', 0, len));
+  var buf = fs.readFileSync('index.html', 'utf8');
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
